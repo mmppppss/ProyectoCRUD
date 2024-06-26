434b850b9ac1c945e5f26c69a82174f45c99d315
mmppppss <ppozosoliz@gmail.com>
2024-06-26 01:17:49 -0400

    add Create GUI

--- src/proyectocrud/CRUD/Create.java
+++ src/proyectocrud/CRUD/Create.java
@@ -1,25 +1,17 @@
 package proyectocrud.CRUD;
 
-import proyectocrud.Gui;
+import javax.swing.UIManager;
 
 /**
  *
  * @author mmppppss
  */
-public class Create extends javax.swing.JPanel {
+public class Create extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Create
-     */
     public Create() {
+        
         initComponents();
-    }
-    public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Create().setVisible(true);
-            }
-        });
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
     }
 
     /**
@@ -31,71 +23,178 @@ public class Create extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        saveButton = new javax.swing.JButton();
+        cancelButton = new javax.swing.JButton();
+        logo = new javax.swing.JLabel();
+        titleField = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        authorFiled = new javax.swing.JTextField();
+        categoriaList = new javax.swing.JComboBox<>();
+        descripcionField = new javax.swing.JTextField();
 
-        setBackground(new java.awt.Color(40, 40, 40));
-        setForeground(new java.awt.Color(251, 241, 199));
-        setMaximumSize(new java.awt.Dimension(720, 480));
-        setMinimumSize(new java.awt.Dimension(720, 480));
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton2.setBackground(new java.awt.Color(29, 32, 33));
-        jButton2.setFont(new java.awt.Font("Source Code Pro", 0, 14)); // NOI18N
-        jButton2.setForeground(new java.awt.Color(168, 153, 132));
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectocrud/assets/save.png"))); // NOI18N
-        jButton2.setText("GUARDAR");
-        jButton2.setFocusPainted(false);
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        jPanel1.setBackground(new java.awt.Color(40, 40, 40));
+        jPanel1.setForeground(new java.awt.Color(251, 241, 199));
+        jPanel1.setMaximumSize(new java.awt.Dimension(720, 480));
+        jPanel1.setMinimumSize(new java.awt.Dimension(720, 480));
+        jPanel1.setPreferredSize(new java.awt.Dimension(720, 480));
+
+        saveButton.setBackground(new java.awt.Color(29, 32, 33));
+        saveButton.setFont(new java.awt.Font("Source Code Pro", 0, 14)); // NOI18N
+        saveButton.setForeground(new java.awt.Color(168, 153, 132));
+        saveButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectocrud/assets/save.png"))); // NOI18N
+        saveButton.setText("GUARDAR");
+        saveButton.setFocusPainted(false);
+        saveButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                saveButtonActionPerformed(evt);
             }
         });
 
-        jButton3.setBackground(new java.awt.Color(29, 32, 33));
-        jButton3.setFont(new java.awt.Font("Source Code Pro", 0, 14)); // NOI18N
-        jButton3.setForeground(new java.awt.Color(168, 153, 132));
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectocrud/assets/cancel.png"))); // NOI18N
-        jButton3.setText("CANCELAR");
-        jButton3.setFocusPainted(false);
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        cancelButton.setBackground(new java.awt.Color(29, 32, 33));
+        cancelButton.setFont(new java.awt.Font("Source Code Pro", 0, 14)); // NOI18N
+        cancelButton.setForeground(new java.awt.Color(168, 153, 132));
+        cancelButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyectocrud/assets/cancel.png"))); // NOI18N
+        cancelButton.setText("CANCELAR");
+        cancelButton.setFocusPainted(false);
+        cancelButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                cancelButtonActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap(468, Short.MAX_VALUE)
-                .addComponent(jButton3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton2)
+        logo.setFont(new java.awt.Font("Source Code Pro Black", 0, 24)); // NOI18N
+        logo.setForeground(new java.awt.Color(235, 219, 178));
+        logo.setText("Informatica Blog Dashboard");
+
+        titleField.setBackground(new java.awt.Color(60, 56, 54));
+        titleField.setFont(new java.awt.Font("Source Code Pro", 0, 14)); // NOI18N
+        titleField.setForeground(getForeground());
+        titleField.setToolTipText("Titulo del Articulo");
+
+        jTextArea1.setBackground(new java.awt.Color(60, 56, 54));
+        jTextArea1.setColumns(20);
+        jTextArea1.setForeground(getForeground());
+        jTextArea1.setRows(5);
+        jTextArea1.setPreferredSize(new java.awt.Dimension(272, 100));
+        jScrollPane1.setViewportView(jTextArea1);
+
+        authorFiled.setBackground(new java.awt.Color(60, 56, 54));
+        authorFiled.setFont(new java.awt.Font("Source Code Pro", 0, 14)); // NOI18N
+        authorFiled.setForeground(getForeground());
+        authorFiled.setToolTipText("Titulo del Articulo");
+
+        categoriaList.setBackground(new java.awt.Color(60, 56, 54));
+        categoriaList.setForeground(getForeground());
+        categoriaList.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Programacion", "Hacking", "Noticias", "Otros" }));
+        categoriaList.setPreferredSize(new java.awt.Dimension(129, 24));
+
+        descripcionField.setBackground(new java.awt.Color(60, 56, 54));
+        descripcionField.setFont(new java.awt.Font("Source Code Pro", 0, 14)); // NOI18N
+        descripcionField.setForeground(getForeground());
+        descripcionField.setToolTipText("Titulo del Articulo");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(titleField)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(logo)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
+                                .addComponent(cancelButton)))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(categoriaList, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(saveButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(authorFiled, javax.swing.GroupLayout.PREFERRED_SIZE, 257, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(descripcionField)))
                 .addGap(20, 20, 20))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(saveButton, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cancelButton, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(logo))
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(432, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(categoriaList, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(titleField, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(authorFiled, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(descripcionField, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(20, 20, 20)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 278, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(20, Short.MAX_VALUE))
         );
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void saveButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveButtonActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_saveButtonActionPerformed
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    private void cancelButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelButtonActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+    }//GEN-LAST:event_cancelButtonActionPerformed
 
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Create.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Create.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Create.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Create.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Create().setVisible(true);
+            }
+        });
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
+    private javax.swing.JTextField authorFiled;
+    private javax.swing.JButton cancelButton;
+    private javax.swing.JComboBox<String> categoriaList;
+    private javax.swing.JTextField descripcionField;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JLabel logo;
+    private javax.swing.JButton saveButton;
+    private javax.swing.JTextField titleField;
     // End of variables declaration//GEN-END:variables
 }
